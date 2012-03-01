5916177f73a6ea5ddb2dec7c3a68cd41ee0df2f8
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-03-01 14:38:29 -0800

    Moved Arc1 to Arc to a single file

---
+++ warc/arc.py
@@ -0,0 +1,97 @@
+"""
+Provides support for Arc v1 files. 
+
+:copyright: (c) 2012 Internet Archive
+"""
+
+from .utils import CaseInsensitiveDict
+
+class ArcHeader(CaseInsensitiveDict):
+    """
+    Holds fields from an Arc V1 or V2 header.
+    V1 header fields are
+
+        * url
+        * ip_address
+        * date
+        * content_type
+        * length (length of the n/w doc in bytes)
+
+    V2 header fields are 
+
+        * url
+        * ip_address
+        * date (date of archival)
+        * content_type 
+        * result_code (response code)
+        * checksum 
+        * location
+        * offset (offset from beginning of file to recrod)
+        * filename (name of arc file)
+        * length (length of the n/w doc in bytes)
+
+    """
+    def __init__(self, url = "",  ip_address = "",  date = "",  content_type = "",  
+                 result_code = "",  checksum = "",  location = "",  offset = "",  filename = "",  length = ""):
+        CaseInsensitiveDict.__init__(self, 
+                                     url = url, 
+                                     ip_address = ip_address,
+                                     date = date,
+                                     content_type = content_type,
+                                     result_code = result_code,
+                                     checksum = checksum,
+                                     location = location,
+                                     offset = offset,
+                                     filename = filename,
+                                     length = length)
+    @property
+    def url(self):
+        return self["url"]
+    
+    @property
+    def ip_address(self):
+        return self["ip_address"]
+    
+    @property
+    def date(self):
+        return self["date"]
+    
+    @property
+    def content_type(self):
+        return self["content_type"]
+    
+    @property
+    def result_code(self):
+        return self["result_code"]
+    
+    @property
+    def checksum (self):
+        return self["checksum"]
+    
+    @property
+    def location(self):
+        return self["location"]
+    
+    @property
+    def offset(self):
+        return self["offset"]
+    
+    @property
+    def filename(self):
+        return self["filename"]
+    
+    @property
+    def length(self):
+        return int(self["length"])
+
+        
+class ArcRecord(object):
+    def __init__(self, header = None, payload = None, headers = {}):
+        if not (header or headers):
+            raise TypeError("Can't write create an Arc1 record without a header")
+        self.header = header or ArcHeader(**headers)
+        self.payload = payload
+        
+        
+        
+    
--- warc/test_arc.py
+++ warc/test_arc.py
@@ -1,22 +1,20 @@
-from . import arc1
+from . import arc
 
 import pytest
 
 def test_create_arc1_header():
     "Make sure Header can be created only with expected fields"
-    with pytest.raises(TypeError):
-        arc1.Arc1Header()
 
     with pytest.raises(TypeError):
-        arc1.Arc1Header(test="1234")
+        arc.ArcHeader(test="1234")
     
 def test_arc1_header_attributes():
     "Make sure that Arc header fields are accessible as attributes"
-    header = arc1.Arc1Header(url = "http://www.archive.org",
-                             ip_address = "127.0.0.1", 
-                             date = "20120301093000", 
-                             content_type = "text/html", 
-                             length = "500")
+    header = arc.ArcHeader(url = "http://www.archive.org",
+                            ip_address = "127.0.0.1", 
+                            date = "20120301093000", 
+                            content_type = "text/html", 
+                            length = "500")
     
     assert header.url == "http://www.archive.org"
     assert header.ip_address == "127.0.0.1"
