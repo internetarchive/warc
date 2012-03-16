bdaf0ada662d320d18eb5ec038eeb8cca91799d2
Anand Chitipothu <anand@archive.org>
2012-03-16 10:48:49 +0530

    Added ARC to the description, fixed the LICENSE link and copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'warc'
-copyright = u'2012, Anand Chitipothu'
+copyright = u'2012, Internet Archive'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -178,8 +178,8 @@ htmlhelp_basename = 'warcdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'warc.tex', u'warc Documentation',
-   u'Anand Chitipothu', 'manual'),
+  ('index', 'warc.tex', u'WARC Documentation',
+   u'Internet Archive', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'warc', u'warc Documentation',
-     [u'Anand Chitipothu'], 1)
+    ('index', 'warc', u'WARC Documentation',
+     [u'Internet Archive'], 1)
 ]
