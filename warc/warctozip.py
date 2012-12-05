1a22358ffe903403540897ecd5681468edadae98
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-12-05 22:26:58 +0000

    Now roughly working warctozip utility, although the response headers are not transferred to the zip.

--- warc/warctozip.py
+++ warc/warctozip.py
@@ -21,16 +21,27 @@ def warc_to_zip():
     warcfile = sys.argv[1]
     zipout = sys.argv[2]
 
-    file = zipfile.ZipFile(zipout, "w")
+    file = zipfile.ZipFile(zipout, "w", zipfile.ZIP_DEFLATED, True)
 
     f = warc.WARCFile( warcfile, "rb" )
     for record in f:
-        print record.header.keys()
+        print "------------"
+        for key in record.header.keys():
+            print key,record.header[key]
         if record.header.has_key('warc-target-uri'):
             u = urlparse(record['WARC-Target-URI'])
-            name = "{}{}".format(u.scheme, u.path)
-            r = httpparse(record.payload)
-            file.writestr(name, r.read())
+            name = "{}/{}/{}".format(u.scheme, u.netloc, u.path )
+            if record['content-type'] == "application/http;msgtype=response":
+                r = httpparse(record.payload)
+                file.writestr(name, r.read())
+            elif record['content-type'] == "application/http;msgtype=request":
+                print "payload:",record.payload
+                print "Skipping request record", record['WARC-Target-URI']
+                file.writestr("{}-request".format(name), record.payload.read())
+            else:
+                print "Skipping record", record['WARC-Target-URI']
+                file.writestr(name, record.payload.read())
+
 
     file.close()
 
